990614088393b7a0b8e811469fd5b1c7329a9967
Whoaskedgaming <143097700+Whoaskedgaming@users.noreply.github.com>
2025-11-05 11:39:21 -0600

    Attempted to import html css and js files ove from codehs

---
+++ data.js
@@ -0,0 +1,387 @@
+export const tweetsData = [
+    {
+        handle: `@ElonMuskDickSucker`,
+        profilePic: `https://i.seadn.io/gae/OE3aqt6IQ4ohL9bYfmFe7yn12tkTH04FOJKTy91ttW5GzDgRjhrAAihbf2FgOh5HQBb8duQveUBpV-Owk-HFRsQyn5TeJJS2JPot?auto=format&dpr=1&w=1000`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },
+    {
+        handle: `@Elon ✅`,
+        profilePic: `Elon.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `We are renaming earth to meme land, we are meme!!! make earth great again!!!`,
+        replies: [
+            
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+    {
+        handle: `@c00lkidd`,
+        profilePic: `c00lkid.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `l0l1p0p!!11 l0l1p0p!!11 l0l1p0p!!11 l0l1p0p!!11`,
+        replies: [
+            {
+                handle: `@1x1x1x1`,
+                profilePic: `1ex4.jpeg`,
+                tweetText: `shut up & fuck you`,
+            },
+            {
+                handle: `@007n7`,
+                profilePic: `007n7&coolkid.jpg`,
+                tweetText: `PLZ, come back home son`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'f579b7b3-5600-4fc5-9ee3-60c846b121d8',
+    },
+    {
+        handle: `@Elon ✅`,
+        profilePic: `Elon.jpg`,
+        likes: 5,
+        retweets: 234,
+        tweetText: `now I need volunteers for a mission to Build a time machine, Alot Of experience and faith necessary!!!`,
+        replies: [
+            {
+                handle: `@Donald. Trump ✅`,
+                profilePic: `donmugshot.jpeg`,
+                tweetText: `Yes, instead of a wall, a machine in time, SEND THEM BACK TO THE 1800's!!!!`,
+            },
+            {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `./assets/images/chucknorris.jpeg`,
+                tweetText: `I already made one losers😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'e97e8de8-e2d9-421d-9b47-ff4776083c7f',
+    },
+    {
+        handle: `@WINston ✅`,
+        profilePic: `Churchill.jpg`,
+        likes: 2,
+        retweets: 2,
+        tweetText: `UK parliament pls forgive me for being racist!!!! `,
+        replies: [
+            {
+                handle: `@Jefferson Finis Davis ✅`,
+                profilePic: `confedjeff.jpg`,
+                tweetText: `you should secede from UK, like me.
+                            `,
+            },
+            {
+                handle: `@stalin ✅`,
+                profilePic: `stalinslove.jpg`,
+                tweetText: `TO THE GULAG`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '027fc788-ba5a-4edc-bbab-2568468b9184',
+    },
+    {
+        handle: `@KanyeWest✅`,
+        profilePic: `https://www.ledr.com/colours/white.jpg`,
+        likes: 56789,
+        retweets: 69420,
+        tweetText: 'After some deep self reflection i have realized i am not a nazi.',
+        replies: [
+            {
+                handle: `@Elon✅`,
+                profilePic: `Elon.jpg`,
+                tweetText: `Tratior!!!!`,
+            },
+            
+            
+            ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '2be9fe95-23f7-4de6-a88c-4cd5828a20d4',
+    },
+    {
+        handle: `@MalumCeado ✅` ,
+        profilePic: `malumm.jfif`,
+        likes: 40000,
+        retweets: 500000,
+        tweetText: ` <img src="BOLTT.png"> PLEASE TRY MY GAME, IM THE MAIN CHARACTER`,
+        replies: [
+            {
+                handle: `@GREAT UNCLEAN ONE`, 
+                profilePic: `nurge.jpg`,
+                tweetText: `WHY DID YOU KILL MY MINIONS AND ME BUT NOT KHORNE`,
+            },
+            {
+                handle: `KHORNE`,
+                profilePic: `https://codehs.com/uploads/51210771b497d015c35a7cf4329267fc`,
+                tweetText: `Get Better rot god 💀💀💀`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'fbc3a57c-8716-49fb-bc45-9314e21764b8',
+    },
+    {
+        handle: `@WilliamIssacIIV `,
+        profilePic: `brit.png`,
+        likes: 2,
+        retweets: 2,
+        tweetText: `Enjoying this delectable spotted dick at the moment. God save the queen!! <img src="spotted.png"> `,
+        replies: [
+            {
+                handle: ` @TrueRedPatriot778 `,
+                profilePic: ` redneck.png `,
+                tweetText: ` THaT LoOks LIkE SHhhHh!t `,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3da679e3-63ba-439c-9235-e313948ad440',
+    },
+    {
+        handle: `@NOT_SCP`,
+        profilePic: `SCP.png`,
+        likes: 0,
+        retweets: 50000000000,
+        tweetText: ` <img src="SCPCB.png"> GUESS WHAT, still no multiplayer
+        but we made bug fixes`,
+        replies: [
+            {
+                handle: `@Chaos `,
+                profilePic: `Chaos.png`,
+                tweetText: ` good to know`,
+            },
+            {
+                handle: `@noober `,
+                profilePic: ``,
+                tweetText: ` good to know`,
+            },
+            {
+                handle: `@@BloodyS1asher`,
+                profilePic: `Jason.png`,
+                tweetText: ` ...... `,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '2a9f5879-9a3f-45a8-afa7-26f2f9fd8404',
+    },
+    {
+        handle: `@MEME_lOver `,
+        profilePic: `SIMP.png`,
+        likes: 5,
+        retweets: 234,
+        tweetText: `MEMEs for the MEME gods
+         <img src="https://codehs.com/uploads/78658d3c1e8ec0d9c5b333f0b5235c9d">
+         <img src="https://codehs.com/uploads/8f7b8e1b725613cd72b14152aa22acac">
+         <img src="https://codehs.com/uploads/ddaa3a33fcb63605fc80ef2084fdf277">`,
+        replies: [
+            {
+                handle: `@Donald. Trump ✅`,
+                profilePic: `donmugshot.jpeg`,
+                tweetText: `UN FUNNY DEPORTED!!!!!!!!!!!!!!!!!!!!!!!`,
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted. png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@BloodyS1asher`,
+                profilePic: `Jason.png`,
+                tweetText: ` ...... `,
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `[MODERATED]`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted. png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+            {
+                handle: `@bootylicker45`,
+                profilePic: `spotted.png`,
+                tweetText: `let me lick ur booty`
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'a816bed1-5c4f-4fb3-a6d8-5595d3cf67ed',
+    },
+    {
+        handle: `@idksterling`,
+        profilePic: `mason.png`,
+        likes: 69,
+        retweets: 5,
+        tweetText: ` <img src="download.png"> meowl my fav`,
+        replies: [
+            {
+                handle: `@Joe Bartalozi  `,
+                profilePic: `joe.png`,
+                tweetText: `Oh when the saints....`,
+            },
+            {
+                handle: `@Ray William Johnson `,
+                profilePic: `ray.png`,
+                tweetText: `go marching innnnn!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'd20fab49-e386-48d8-93d7-e3e18461616e',
+    },
+    {
+        handle: `@Bl00dyS1asher`,
+        profilePic: `Jason.png`,
+        likes: 3,
+        retweets: 7789,
+        tweetText: ` <img src="https://media1.tenor.com/m/AVf3jE-4h4EAAAAC/halloween-jason.gif"> `,
+        replies: [
+            {
+                handle: `@c00lkidd`,
+                profilePic: `c00lkid.png`,
+                tweetText: `ur so c00l....`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'e8592720-ea0d-4996-bebf-858b01092bdc',
+    },
+    {
+        handle: `@Slenderguy002`,
+        profilePic: `https://codehs.com/uploads/a3a70067eddbf14450f5d0f7794102dc`,
+        likes: 0,
+        retweets: 0,
+        tweetText: `find my pages`,
+        replies: [
+            {
+                handle: `@BloodyS1asher`,
+                profilePic: `Jason.png`,
+                tweetText: `.....`,
+            },
+            {
+                handle: `@c00lkidd`,
+                profilePic: `c00lkid.png`,
+                tweetText: `AAAAAAAAA`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'f7029b2b-72a7-44df-a662-e2856ea69911',
+    },
+
+]
---
+++ feed.js
@@ -0,0 +1,226 @@
+import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+let storedTweetsData = JSON.parse(localStorage.getItem('tweetsData')) || tweetsData
+
+if (storedTweetsData) {
+    localStorage.setItem('tweetsData', JSON.stringify(storedTweetsData));
+}
+
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    }
+    else if (e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if (e.target.dataset.reply) {
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if (e.target.id === 'tweet-btn') {
+        handleTweetBtnClick()
+    }
+    else if (e.target.dataset.comment) {
+        handleReplyTextBtnClick(e.target.dataset.comment)
+    }
+    else if (e.target.dataset.delete) {
+        handleDelete(e.target.dataset.delete)
+    }
+})
+
+function handleDelete(tweetId) {
+    let tweetData = JSON.parse(localStorage.getItem('tweetsData')) || []
+    tweetData = tweetData.filter(function (tweet) {
+        return tweet.uuid !== tweetId
+    })
+
+    localStorage.setItem('tweetsData', JSON.stringify(tweetData))
+    render()
+
+}
+
+function handleLikeClick(tweetId) {
+    let tweetsData = JSON.parse(localStorage.getItem('tweetsData'))
+
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked) {
+        targetTweetObj.likes--
+    }
+    else {
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+    render()
+}
+
+function handleRetweetClick(tweetId) {
+    let tweetsData = JSON.parse(localStorage.getItem('tweetsData'))
+
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--
+    }
+    else {
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+    render()
+}
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleReplyTextBtnClick(tweetId) {
+    console.log("T ID: " + tweetId)
+    let tweetsData = JSON.parse(localStorage.getItem('tweetsData'))
+    let replyInput = document.getElementById(`reply-input-${tweetId}`).value;
+    console.log(replyInput)
+
+    if (replyInput) {
+        const targetTweetObj = tweetsData.find(function (tweet) {
+            return tweet.uuid === tweetId;
+        })
+
+        targetTweetObj.replies.unshift({
+            handle: `@Guest1337`,
+            profilePic: `AOL.jpeg`,
+            tweetText: replyInput,
+        });
+
+        localStorage.setItem('tweetsData', JSON.stringify(tweetsData));
+        replyInput = '';
+
+        render(tweetsData);
+    }
+}
+
+function handleTweetBtnClick() {
+    const tweetInput = document.getElementById('tweet-input')
+    let tweetsData = JSON.parse(localStorage.getItem('tweetsData')) || []
+
+    if (tweetInput.value) {
+        tweetsData.unshift({
+            handle: `@Guest1337`,
+            profilePic: `AOL.jpeg`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+
+        localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+        render()
+        tweetInput.value = ''
+    }
+
+}
+
+function getFeedHtml() {
+    let tweetsData = JSON.parse(localStorage.getItem('tweetsData'))
+    let feedHtml = ``
+
+    tweetsData.forEach(function (tweet) {
+
+        let likeIconClass = tweet.isLiked ? 'liked' : ''
+
+        let retweetIconClass = tweet.isRetweeted ? 'retweeted' : ''
+
+        let repliesHtml = ''
+
+        if (tweet.replies.length > 0) {
+            tweet.replies.forEach(function (reply) {
+                repliesHtml += `
+                <div class="tweet-reply">
+                    <div class="tweet-inner">
+                        <img src="${reply.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${reply.handle}</p>
+                            <p class="tweet-text">${reply.tweetText}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+            })
+        }
+
+        repliesHtml += `
+            <div class="tweet-reply">
+                <textarea id='reply-input-${tweet.uuid}' placeholder="Write your reply..."></textarea>
+                <button class='reply-button' data-comment="${tweet.uuid}">Reply</button>
+            </div>
+        `
+
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart ${likeIconClass}"
+                            data-like="${tweet.uuid}"
+                            ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet ${retweetIconClass}"
+                            data-retweet="${tweet.uuid}"
+                            ></i>
+                            ${tweet.retweets}
+                        </span>
+                        
+                    </div>
+                    <div class="delete-container">
+                            <i class="fa-solid fa-x delete-icon"
+                            data-delete="${tweet.uuid}"
+                            ></i>
+                    </div> 
+                </div>            
+            </div>
+            <div class="hidden" id="replies-${tweet.uuid}">
+                ${repliesHtml}
+            </div>   
+        </div>
+        `
+    })
+    return feedHtml
+}
+
+function renderReplyTexts() {
+    const replyButtons = document.querySelectorAll('.reply-button')
+    replyButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const tweetId = button.dataset.tweetId
+            const replyInput = document.getElementById(`reply-input-${tweetId}`)
+            handleReplyTextBtnClick(tweetId, replyInput)
+        })
+    })
+}
+
+function render() {
+    document.getElementById('feed').innerHTML = getFeedHtml()
+
+    renderReplyTexts()
+}
+
+render()
